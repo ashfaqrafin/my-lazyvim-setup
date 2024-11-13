5481b7a0efe5e7802a9d01e5f7637b84a87a5520
Ashfaq Khan <82702762+ashfaqrafin@users.noreply.github.com>
2024-11-13 06:47:04 +0600

    first commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.cmd("set shiftwidth=4")
+require("lazyvim").setup({
+  picker = "telescope",
+})
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "float",
+      width = 50,
+      border="rounded",
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+  },
+}
